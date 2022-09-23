025d8d59882fd2c6c517785c4f614c253abc7697
Coqicoqi <subuhtime@gmail.com>
2022-09-23 22:33:47 +0700

    first commit

---
+++ dist/js/script.js
@@ -0,0 +1,57 @@
+//navbar-fixed
+window.onscroll = function () {
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+    const toTop = document.querySelector('#to-top');
+
+    if (window.pageYOffset > fixedNav) {
+    header.classList.add('navbar-fixed');
+    //hidden
+    toTop.classList.remove('hidden');
+    toTop.classList.add('flex');
+    } else {
+        header.classList.remove('navbar-fixed');
+        //scroll show
+        toTop.classList.remove('flex');
+        toTop.classList.add('hidden');
+    }
+};
+
+// navbar-hamburger
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
+
+hamburger.addEventListener('click', function()  {
+    hamburger.classList.toggle('hamburger-active');
+    navMenu.classList.toggle('hidden');
+});
+
+//Klik di luar Hamburger
+window.addEventListener('click', function(e) {
+    if (e.target != hamburger && e.target != navMenu) {
+        hamburger.classList.remove('hamburger-active');
+        navMenu.classList.add('hidden');
+    }
+});
+
+//DarkMode Toggle
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function () {
+    if (darkToggle.checked) {
+        html.classList.add('dark');
+        localStorage.theme = 'dark';
+    } else {
+        html.classList.remove('dark');
+        localStorage.theme = 'light';
+    }
+});
+
+// On page load or when changing themes, best to add inline in `head` to avoid FOUC
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) 
+&& window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darkToggle.checked = true;
+  } else {
+    darkToggle.checked = false;
+  } 
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ['index.html'],
+  darkMode: 'class',
+  theme: {
+    container: {
+        center: true,
+        padding: '16px',
+    },
+    extend: {
+      color: {
+        primary: '#14b8a6',
+        secondary: '#64748b',
+        dark:'#737373',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+};
